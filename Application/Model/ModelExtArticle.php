673bbdeaa01cce7f261e2eaf862d6e20ba806c47
OxidMarketplace <142104076+OxidMarketplace@users.noreply.github.com>
2023-08-12 20:32:00 +0200

    Update ModelExtArticle.php textin

--- Application/Model/ModelExtArticle.php
+++ Application/Model/ModelExtArticle.php
@@ -1,1 +1,17 @@
-asasa
+<?php
+ # Example for module with own namespace
+
+ namespace MyVendor\MyModuleNamespace\Application\Model;
+
+ class MyModuleArticle extends MyModuleArticle_parent
+ {
+     public function getSize()
+     {
+         $originalSize = parent::getSize();
+
+         //double the size
+         $newSize = 2 * $originalSize;
+
+         return $newSize;
+     }
+ }
