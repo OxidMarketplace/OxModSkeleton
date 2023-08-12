9da54b376ba66791474be021d277975e8d0aee45
OxidMarketplace <142104076+OxidMarketplace@users.noreply.github.com>
2023-08-12 17:01:07 +0200

    empty metadata.php

--- metadata.php
+++ metadata.php
@@ -1,1 +1,41 @@
-sdsds
+/**
+ *
+ * @link      https://github.com/OxidMarketplace
+ * @copyright (C) Jens Hergert
+ * @version   1.1
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '6-3.1';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'oxmodskeleton',
+    'title'       => 'Oxid Module Skeleton with Namespaces',
+    'description' => array(
+        'de' => 'Oxid Module Skeleton with Namespaces',
+        'en' => 'Oxid Module Skeleton with Namespaces',
+    ),
+    'thumbnail'     => 'oxmodskeleton.png',
+    'version'       => '6-3.1',
+    'author'        => 'Jens Hergert',
+    'url'           => 'https://github.com/OxidMarketplace',
+    'email'         => 'kenmarall@hotmail.com',
+
+    'extend'        => array(),
+
+    'controllers'   => array(),
+    
+    'templates'     => array(),
+
+    'events'        => array(),
+    
+    'blocks'        => array(),
+    
+    'settings'      => array()
+);
+
